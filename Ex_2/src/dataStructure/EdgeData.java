661238b826d8293211a251b987cacf5b5134aee8
yoantan <yonatan1231230@gmail.com>
2019-12-16 17:18:46 +0200

    first commit

---
+++ Ex_2/src/dataStructure/EdgeData.java
@@ -0,0 +1,47 @@
+package dataStructure;
+
+public class EdgeData implements edge_data {
+	
+	@Override
+	public int getSrc() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getDest() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getWeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setInfo(String s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getTag() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setTag(int t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Ex_2/src/dataStructure/NodeData.java
@@ -0,0 +1,61 @@
+package dataStructure;
+
+import utils.Point3D;
+
+public class NodeData implements node_data {
+
+	@Override
+	public int getKey() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Point3D getLocation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setLocation(Point3D p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getWeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setWeight(double w) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setInfo(String s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getTag() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setTag(int t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
