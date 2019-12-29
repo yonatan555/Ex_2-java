97073c43e607f3a5bc73b3fa0ea8b47530f909de
yoantan <yonatan1231230@gmail.com>
2019-12-29 21:06:51 +0200

    started gui

--- Ex_2/src/gui/Main.java
+++ Ex_2/src/gui/Main.java
@@ -11,19 +11,19 @@ public class Main {
 	public static void main(String[] args) {
 		DGraph temp = new DGraph();
 
-		Point3D m = new Point3D(1, 3, 4);
-		Point3D m1 = new Point3D(2, 4, 5);
-		Point3D m2 = new Point3D(3, 5, 6);
-		Point3D m3 = new Point3D(4, 6, 7);
-		Point3D m4 = new Point3D(2, 6, 7);
-		// Point3D m5 = new Point3D (6,6,7);
+		Point3D m = new Point3D(100, 352, 4);
+		Point3D m1 = new Point3D(400, 210, 5);
+		Point3D m2 = new Point3D(120, 300, 6);
+		Point3D m3 = new Point3D(130,200, 7);
+		Point3D m4 = new Point3D(340, 80, 7);
+		 //Point3D m5 = new Point3D (6,6,7);
 
 		NodeData a = new NodeData(1, m);
 		NodeData b = new NodeData(2, m1);
 		NodeData c = new NodeData(3, m2);
 		NodeData d = new NodeData(4, m3);
 		NodeData e = new NodeData(5, m4);
-		// NodeData f = new NodeData(6,2.5,m5);
+		 //NodeData f = new NodeData(6,2.5,m5);
 
 		temp.addNode(a);
 		temp.addNode(b);
@@ -32,9 +32,13 @@ public class Main {
 		temp.addNode(e);
 
 		temp.connect(a.getKey(), b.getKey(), 2);
-		temp.connect(b.getKey(), c.getKey(), 2);
-		temp.connect(c.getKey(), d.getKey(), 2);
-		temp.connect(d.getKey(), e.getKey(), 2);
+		temp.connect(c.getKey(), e.getKey(), 10);
+		temp.connect(b.getKey(), c.getKey(), 6);
+		temp.connect(c.getKey(), d.getKey(), 1);
+		temp.connect(d.getKey(), e.getKey(), 5);
+		temp.connect(e.getKey(), a.getKey(), 3);
+		
+		
 		
 		graph_gui window = new graph_gui(temp);
 		
--- Ex_2/src/gui/graph_gui.java
+++ Ex_2/src/gui/graph_gui.java
@@ -1,23 +1,32 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.Event;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Menu;
 import java.awt.MenuBar;
 import java.awt.MenuItem;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.util.LinkedList;
-
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.awt.event.*;
+import java.awt.*;
 import javax.swing.JFrame;
 
 import dataStructure.edge_data;
 import dataStructure.graph;
+import dataStructure.main;
 import dataStructure.node_data;
+import algorithms.Graph_Algo;
+import algorithms.graph_algorithms;
 
-public class graph_gui extends JFrame implements ActionListener, MouseListener {
+public class graph_gui extends JFrame  implements ActionListener {
+
 	graph grp;
 
 	public graph_gui() {
@@ -30,73 +39,71 @@ public class graph_gui extends JFrame implements ActionListener, MouseListener {
 	}
 
 	private void initGUI() {
-
+		
+		
 		this.setSize(900, 750);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		MenuBar menuBar = new MenuBar();
-
+		
 		Menu menu = new Menu("Menu");
-		//Menu graph = new Menu("Graph");
-
 		menuBar.add(menu);
-		//menuBar.add(graph);
-
 		this.setMenuBar(menuBar);
 
 		MenuItem item1 = new MenuItem("Paint_Graph");
 		item1.addActionListener(this);
 		menu.add(item1);
-		
-		/*MenuItem item1 = new MenuItem("Save");
-		item1.addActionListener
-		(this);
-		MenuItem item2 = new MenuItem("Load");
+
+		MenuItem item2 = new MenuItem("Save");
 		item2.addActionListener(this);
-		MenuItem item3 = new MenuItem("Is Connected");
-		item1.addActionListener(this);
-		MenuItem item4 = new MenuItem("Shortest Path");
-		item2.addActionListener(this);
-		MenuItem item5 = new MenuItem("Shortest");
-		item1.addActionListener(this);
-		MenuItem item6 = new MenuItem("Paint Graph");
-		item2.addActionListener(this);
-		*/
-		/*menu.add(item2);
-		graph.add(item6);
-		graph.add(item3);
-		graph.add(item4);
-		graph.add(item5);
-		 */
-		//this.addMouseListener(this);
+		menu.add(item2);
+
+		MenuItem item3 = new MenuItem("Load");
+		item3.addActionListener(this);
+		menu.add(item3);
+
+		MenuItem item4 = new MenuItem("Is Connected");
+		item4.addActionListener(this);
+		menu.add(item4);
+
+		MenuItem item5 = new MenuItem("Shortest_Path_Dist");
+		item5.addActionListener(this);
+		menu.add(item5);
+
+		MenuItem item6 = new MenuItem("Shortest_Path");
+		item6.addActionListener(this);
+		menu.add(item6);
+
+		MenuItem item7 = new MenuItem("TSP");
+		item7.addActionListener(this);
+		menu.add(item7);
+
 	}
 
 	public void paint(Graphics g) {
 
-		
 		super.paint(g);
-		
-		
-		node_data dest = null;
 
+		node_data dest = null;
 		if (this.grp != null) {
 			for (node_data no : this.grp.getV()) {
-				
 				g.setColor(Color.BLUE);
-				g.fillOval(no.getLocation().ix(), no.getLocation().iy(), 10, 10); 							// draw src point
-				g.drawString("" + no.getKey(), no.getLocation().ix(), no.getLocation().iy() + 1); 			// draw the num of src point
+				g.fillOval(no.getLocation().ix(), no.getLocation().iy(), 20, 20); // draw src point
+				g.setFont(new Font("TimesRoman", Font.PLAIN, 25));
+				g.drawString("" + no.getKey(), no.getLocation().ix(), no.getLocation().iy() + 1); // draw the num of src point
+				
 				for (edge_data ed : this.grp.getE(no.getKey())) {
-					
 					dest = this.grp.getNode(ed.getDest());
 					g.setColor(Color.RED);
-					g.drawLine(no.getLocation().ix(), no.getLocation().iy(), dest.getLocation().ix(), 		// draw edge point
+					g.drawLine(no.getLocation().ix(), no.getLocation().iy(), dest.getLocation().ix(), // draw edge point
 							dest.getLocation().iy());
-					g.setColor(Color.CYAN);
-					g.drawString("" + no.getKey(), (no.getLocation().ix() + dest.getLocation().ix()) / 2, 	// draw weight of edge point													
+					g.setColor(Color.black);
+					g.setFont(new Font("TimesRoman", Font.PLAIN, 18));
+					g.drawString("" + ed.getWeight(), (no.getLocation().ix() + dest.getLocation().ix()) / 2, // draw weight of edge point
 							((no.getLocation().iy() + dest.getLocation().iy()) / 2) + 1);
-					g.setColor(Color.YELLOW);																//draw enterance point  
-					g.fillOval(dest.getLocation().ix(), dest.getLocation().iy(), 10,
-							10);
+					g.setColor(Color.YELLOW);
+					g.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+					g.fillOval(dest.getLocation().ix() - 5, dest.getLocation().iy() - 3, 10, 10); // draw enterance point 
 				}
 			}
 		}
@@ -104,41 +111,91 @@ public class graph_gui extends JFrame implements ActionListener, MouseListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-
 		String op = e.getActionCommand();
-
 		if (op.equals("Paint_Graph")) {
+			System.out.println("sss");
+			repaint();
+		}
+		else if (op.equals("Save")) {
+			repaint();
+		}
+		else if (op.equals("Load")) {
+			repaint();
+		}
+		else if (op.equals("Is Connected")) {
+			
+			isConnected();
+			repaint();
+		}
+		else if (op.equals("Shortest_Path_Dist")) {
+			repaint();
+		}
+		else if (op.equals("Shortest_Path")) {
+			repaint();
+		}
+		else if (op.equals("TSP")) {
 			repaint();
 		}
 	}
 
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
+	private void isConnected() {
+		Graph_Algo m = new Graph_Algo();
+		m.init(this.grp);
+		System.out.println(m.isConnected());
 		
+		System.out.println("sss");
 	}
 
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+	private void Save(String file_name) {
+		try {
+			FileOutputStream file = new FileOutputStream(file_name);
+			ObjectOutputStream out = new ObjectOutputStream(file);
+			out.writeObject(this);
+			out.close();
+			file.close();
+			System.out.println("Object has been serialized");
+		} catch (IOException ex) {
+			System.out.println("IOException is caught");
+		}
 	}
 
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+	public void Load(String file_name) { // init from fiile
+
+		Graph_Algo gr = null;
+		try {
+			FileInputStream file = new FileInputStream(file_name);
+			ObjectInputStream in = new ObjectInputStream(file);
+
+			gr = (Graph_Algo) in.readObject();
+
+			in.close();
+			file.close();
+
+			System.out.println("Object has been deserialized");
+			System.out.println(gr.m);
+		} 
+		catch (IOException ex) {
+			System.out.println("IOException is caught");
+		}
+		catch (ClassNotFoundException ex) {
+			System.out.println("ClassNotFoundException is caught");
+		}
 	}
 
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+	private void Shortest_Path_Dist() {
+		Graph_Algo m = new Graph_Algo();
+		m.init(this.grp);
+		m.shortestPathDist(1, 2);
 	}
 
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+	private void Shortest_Path() {
+		Graph_Algo m = new Graph_Algo();
+		m.init(this.grp);
+		m.shortestPath(1, 3);
+	}
+	private void TSP(){
+		Graph_Algo m = new Graph_Algo();
+		m.init(this.grp);
+		m.TSP(1, 3);
 	}
 }
